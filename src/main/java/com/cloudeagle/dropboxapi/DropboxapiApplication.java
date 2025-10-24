84e37472336e52a681cf548324680373b2caacde
ROXAN <roxan@ROXAN.local>
2025-10-24 17:24:54 +0530

    initial project setup

---
+++ src/main/java/com/cloudeagle/dropboxapi/DropboxapiApplication.java
@@ -0,0 +1,13 @@
+package com.cloudeagle.dropboxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropboxapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropboxapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudeagle/dropboxapi/DropboxapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudeagle.dropboxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropboxapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
